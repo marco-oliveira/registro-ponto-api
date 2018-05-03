66f9361bd250b454eb8d095bf8bf4ad43e9b6cab
marco-oliveira <marco-oliveira@live.com>
2018-05-02 22:40:38 -0300

    Criando Entidades JPA

---
+++ src/main/java/com/marcooliveira/pontointeligente/api/enums/PerfilEnum.java
@@ -0,0 +1,9 @@
+package com.marcooliveira.pontointeligente.api.enums;
+
+/**
+ * Created by Marco Antônio on 02/05/2018
+ */
+public enum PerfilEnum {
+    ROLE_ADMIN,
+    ROLE_USUARIO
+}
---
+++ src/main/java/com/marcooliveira/pontointeligente/api/enums/TipoEnum.java
@@ -0,0 +1,13 @@
+package com.marcooliveira.pontointeligente.api.enums;
+
+/**
+ * Created by Marco Antônio on 02/05/2018
+ */
+public enum TipoEnum {
+    INICIO_TRABALHO,
+    TERMINO_TRABALHO,
+    INICIO_ALMOCO,
+    TERMINO_ALMOCO,
+    INICIO_PAUSA,
+    TERMINO_PAUSA
+}
